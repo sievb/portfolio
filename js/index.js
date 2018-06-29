fc25daabce9c72edce434bdca5fd23d91fe56f36
Ben Siev <benjaminsiev@gmail.com>
2018-06-28 17:58:02 -0700

    homepage 95% done, uploading to test anchor tag change

--- js/index.js
+++ js/index.js
@@ -8,11 +8,37 @@
 //     x.className = "navbar";
 //   }
 // }
+var portfolioItems = [
+	{
+		img: "deleteYourself.png",
+		title: "Delete Yourself",
+		desc: "A website dedicated to helping visitors practice good online habits, as well as taking control of their online presence",
+		url: "deleteYourself.html"
+	},
+	{
+		img: "zoldo.png",
+		title: "Fable of Zoldo",
+		desc: "A Legend of Zelda parody that attempts to get players to question why they play games by introducing absurdist aspects",
+		url: "zoldo.html"
+	},
+	{
+		img: "puzzle.png",
+		title: "Puzzle Spectrum",
+		desc: "An app to help demystify autism and the autism spectrum by providing basic information and adding a more human focus",
+		url: "puzzle.html"
+	}
+]
+
+var buttons = $("#portfolioNav button");
+
 var parallax = document.getElementById('landing');
 var parallaxInstance = new Parallax(parallax, {
 	pointerEvents: true,
 	hoverOnly: true
 });
+
+console.log(window.location.pathname);
+console.log(window.location.href);
 
 $(document).ready(function() {
 	$("a").click(function() {
@@ -24,20 +50,44 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#portfolioNav button").click(function() {
-		$("#portfolioNav button.current").removeClass("current");
+	buttons.click(function() {
+		var index = buttons.index(this);
+		var portfolio = $("#portfolio");
+		var image = $("#imgContainer img");
+		var title = $("#portfolio h2");
+		var description = $("#portfolio p");
+		var url = $("#portfolio a");
+
+		$("button.current").removeClass("current");
 		$(this).addClass("current");
+
+		portfolio.fadeOut(200, function() {
+			image.attr("src", "img/" + portfolioItems[index].img);
+			title.text(portfolioItems[index].title);
+			description.text(portfolioItems[index].desc);
+			url.attr("href", portfolioItems[index].url)
+			portfolio.fadeIn(200);
+		});
 	});
 
+	$("#imgContainer").change(function() {
+		var selected = $(this).val();
+		var image = $("#imgContainer img");
+		image.fadeOut("fast", function() {
+			image.attr('src', "img/" + portfolioItems[index].img);
+			image.fadeIn("fast");
+		});
+	})
+
 	$("#landing p").click(function() {
-			parallaxInstance.destroy();
-			$(this).text("Parallax disabled");
-			$(this).addClass("noInteraction")
+		parallaxInstance.destroy();
+		$(this).text("Parallax disabled");
+		$(this).addClass("noInteraction")
 	});
 
 	$("#portfolioText a").hover(function() {
-		$("#portfolioImg").toggleClass("hover");
 		$("#portfolioText").toggleClass("hover");
+		$("#portfolio img").toggleClass("hover");
 		$(this).toggleClass("hover");
 	})
 });
