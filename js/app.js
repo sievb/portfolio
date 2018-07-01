437d885d943fc7c68366427b6660ff198b234628
Ben Siev <benjaminsiev@gmail.com>
2018-06-30 19:02:51 -0700

    parallax working, testing image responsiveness

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 		if(this.hash !== "") {
 			var hash = this.hash;
 			$("html, body").animate({
-				scrollTop: $(hash).offset().top - 75
+				scrollTop: $(hash).offset().top - 50
 			}, 800);
 		}
 	});
