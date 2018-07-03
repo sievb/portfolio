84c94b3ffd448a8578b34af7f6348462e2995392
Ben Siev <benjaminsiev@gmail.com>
2018-07-03 13:18:09 -0700

    fixed linkedin url

--- js/app.js
+++ js/app.js
@@ -1,27 +1,4 @@
 $(document).foundation()
-var portfolioItems = [
-	{
-		img: "deleteYourself.png",
-		title: "Delete Yourself",
-		desc: "A website dedicated to helping visitors practice good online habits, as well as taking control of their online presence",
-		url: "deleteYourself.html"
-	},
-	{
-		img: "zoldo.png",
-		title: "Fable of Zoldo",
-		desc: "A Legend of Zelda parody that attempts to get players to question why they play games by introducing absurdist aspects",
-		url: "zoldo.html"
-	},
-	{
-		img: "puzzle.png",
-		title: "Puzzle Spectrum",
-		desc: "An app to help demystify autism and the autism spectrum by providing basic information and adding a more human focus",
-		url: "puzzle.html"
-	}
-]
-
-var buttons = $("#portfolioNav button");
-
 var parallax = document.getElementById('landing');
 var parallaxInstance = new Parallax(parallax, {
 	pointerEvents: true,
@@ -29,7 +6,6 @@ var parallaxInstance = new Parallax(parallax, {
 });
 
 $(document).ready(function() {
-
 	$("a").click(function() {
 		if(this.hash !== "") {
 			var hash = this.hash;
