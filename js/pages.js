5491e2a0fa88285ee1af31977fca4b9df36b5356
Ben Siev <benjaminsiev@gmail.com>
2018-07-04 18:59:29 -0700

    added class to test scrolling on mobile

--- js/pages.js
+++ js/pages.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     window.addEventListener("scroll", function() {
       if (window.scrollY > 600) {
-        console.log("Scrolled past 500 pixels");
+        $("body").addClass("light");
+      } else {
+        $("body").removeClass("light");
       }
     }, false);
 });
