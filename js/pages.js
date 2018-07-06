727945080abee35fdda5d03756c3004c51d38932
Ben Siev <benjaminsiev@gmail.com>
2018-07-06 16:38:13 -0700

    fixed wrong button placement on index, completed delete.html for now, added js to detect page scroll location and update classes

--- js/pages.js
+++ js/pages.js
@@ -1,10 +1,23 @@
-$(document).foundation()
-$(document).ready(function () {
-    window.addEventListener("scroll", function() {
-      if (window.scrollY > 600) {
-        $("body").addClass("light");
-      } else {
-        $("body").removeClass("light");
-      }
-    }, false);
+$(document).foundation();
+
+$(document).ready(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll > 600) {
+    $("body").addClass("light");
+    $(".divider span").addClass("light");
+  } else {
+    $("body").removeClass("light");
+    $(".divider span").removeClass("light");
+  };
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 600) {
+      $("body").addClass("light");
+      $(".divider span").addClass("light");
+    } else {
+      $("body").removeClass("light");
+      $(".divider span").removeClass("light");
+    };
+  });
 });
